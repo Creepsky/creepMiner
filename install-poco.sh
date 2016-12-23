a2f36fd69f5f1d51b90d7afcf58481899785ad15
Creepsky <creepsky@gmail.com>
2016-12-23 21:26:11 +0100

    POCO install script, resolves #28

--- install-poco.sh
+++ install-poco.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://pocoproject.org/releases/poco-1.7.6/poco-1.7.6.tar.gz
-tar -xzvf poco-1.7.6.tar.gz
-cd poco-1.7.6 && ./configure --prefix=/usr --no-tests --no-samples && make && sudo make install
+wget https://pocoproject.org/releases/poco-1.7.6/poco-1.7.6-all.tar.gz
+tar -xzvf poco-1.7.6-all.tar.gz
+cd poco-1.7.6-all && ./configure --no-tests --no-samples && make && sudo ldconfig && sudo make install
