9de67609818ca0c2af5ac7db79fe963152bf2147
Creepsky <creepsky@gmail.com>
2017-03-03 19:51:02 +0100

    New version 1.5.0

--- src/Declarations.cpp
+++ src/Declarations.cpp
@@ -1,7 +1,7 @@
 #include "Declarations.hpp"
 #include <string>
 
-const Burst::Version Burst::Settings::ProjectVersion = { 1, 4, 6 };
+const Burst::Version Burst::Settings::ProjectVersion = { 1, 5, 0 };
 
 #if defined MINING_CUDA
 const Burst::ProjectData Burst::Settings::Project = { "creepMiner CUDA", ProjectVersion };
