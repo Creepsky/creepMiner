97c3a268b547e08f736b487cbc5ef7b9146a74e6
Creepsky <creepsky@gmail.com>
2017-05-28 20:17:42 +0200

    Removed fragements of target deadline

--- bin/public/js/settings.js
+++ bin/public/js/settings.js
@@ -9,14 +9,7 @@ function update_settings(config){
     buffer_size.val(config['bufferSizeMB']);
     plot_readers.val(config['maxPlotReaders']);
     submission_max_retry.val(config['submissionMaxRetry']);
-
-    var tdo = config['targetDeadlineObject'];
-    target_deadline_years.val(tdo.years);
-    target_deadline_months.val(tdo.months);
-    target_deadline_days.val(tdo.days);
-    target_deadline_hours.val(tdo.hours);
-    target_deadline_minutes.val(tdo.minutes);
-    target_deadline_seconds.val(tdo.seconds);
+    target_deadline.val(config['targetDeadlineText']);
 
     timeout.val(config['timeout']);
 }
@@ -52,12 +45,7 @@ window.onload = function(evt) {
     buffer_size = $("#buffer-size");
     plot_readers = $("#plot-readers");
     submission_max_retry = $("#submission-max-retry");
-    target_deadline_years = $("#target-deadline-years");
-    target_deadline_months = $("#target-deadline-months");
-    target_deadline_days = $("#target-deadline-days");
-    target_deadline_hours = $("#target-deadline-hours");
-    target_deadline_minutes = $("#target-deadline-minutes");
-    target_deadline_seconds = $("#target-deadline-seconds");
+    target_deadline = $("#target-deadline");
     timeout = $("#timeout");
 
     connect(connectCallback);
