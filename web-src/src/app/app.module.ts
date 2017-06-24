7f5b4928ef1d185634352167d0157d047716414a
hheexx <hexboris@gmail.com>
2017-06-24 16:53:36 +0200

    created status, plotstatus and settings components. Impotred css, images and basic html

--- web-src/src/app/app.module.ts
+++ web-src/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlotFilesComponent } from './plot-files/plot-files.component';
+import { SettingsComponent } from './settings/settings.component';
+import { StatusComponent } from './status/status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlotFilesComponent,
+    SettingsComponent,
+    StatusComponent
   ],
   imports: [
     BrowserModule
