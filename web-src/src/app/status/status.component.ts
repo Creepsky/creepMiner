ab9e078b179a0944bd905f4332ef4f88a8e749cd
hheexx <hexboris@gmail.com>
2017-07-05 12:16:24 +0200

    add new found confirmation to graph immediately

--- web-src/src/app/status/status.component.ts
+++ web-src/src/app/status/status.component.ts
@@ -36,20 +36,30 @@ export class StatusComponent implements OnInit {
       const bestDl = <Array<Array<number>>>nb['bestDeadlines'];
       this.lineChartLabels = bestDl.map(x => x[0]);
       this.lineChartData[0].data = bestDl.map(x => x[1]);
-    })
+    });
+    this.b.newConfirmation$.subscribe(nc => {
+      if (this.lineChartLabels[this.lineChartLabels.length - 1] === this.b.newBlock.block) {
+        console.log('vec ima');
+      } else {
+        console.log('treba push');
+        this.lineChartLabels.push(this.b.newBlock.block);
+        this.lineChartData[0].data.push(this.bestDeadline().deadlineNum);
+        setTimeout(() => { }, 0);
+      }
+    });
   }
 
   round(n: number): number {
     return Math.round(n);
   }
 
-  bestDeadline(): string {
+  bestDeadline(): JSONS.NonceObject {
     const bestDl = Math.min(...this.b.nonces.map(x => x.deadlineNum));
 
     if (bestDl !== Infinity) {
-      return this.b.nonces.filter(x => x.deadlineNum == bestDl)[0].deadline;
+      return this.b.nonces.filter(x => x.deadlineNum == bestDl)[0];
     } else {
-      return '/';
+      return null;
     }
 
   }
