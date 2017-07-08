5c7fa430d7dd8212b7228f4d0ffefa43c8468b3c
theblazehen <theblazehen@gmail.com>
2017-07-08 15:18:05 +0200

    Improve install-poco.sh script
    Run single apt-get command, don't prompt for confirmation (People should read the script before running it anyway), and run make in parallel

--- install-poco.sh
+++ install-poco.sh
@@ -2,6 +2,5 @@
 set -ex
 wget https://pocoproject.org/releases/poco-1.7.6/poco-1.7.6-all.tar.gz
 tar -xzvf poco-1.7.6-all.tar.gz
-sudo apt-get install openssl
-sudo apt-get install libssl-dev
-cd poco-1.7.6-all && ./configure --no-tests --no-samples && make && sudo ldconfig && sudo make install
+sudo apt-get install -y openssl libssl-dev
+cd poco-1.7.6-all && ./configure --no-tests --no-samples && make -j $(nproc) && sudo ldconfig && sudo make install
