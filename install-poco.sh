c287435a83c85f04b47b0ecca1730ccdb162206a
Creepsky <creepsky@gmail.com>
2017-07-16 03:41:28 +0200

    Update install-poco.sh

--- install-poco.sh
+++ install-poco.sh
@@ -2,5 +2,4 @@
 set -ex
 wget https://pocoproject.org/releases/poco-1.7.6/poco-1.7.6-all.tar.gz
 tar -xzvf poco-1.7.6-all.tar.gz
-sudo apt-get install -y openssl libssl-dev
-cd poco-1.7.6-all && ./configure --no-tests --no-samples && make -j $(nproc) && sudo ldconfig && sudo make install
+cd poco-1.7.6-all && ./configure --no-tests --no-samples --omit=Data/ODBC,Data/MySQL && make -j $(nproc) && sudo ldconfig && sudo make install
