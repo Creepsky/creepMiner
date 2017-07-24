bebb43fc81355816e0ee4dcb52087cc50fd952ef
Creepsky <creepsky@gmail.com>
2017-07-24 07:56:14 +0200

    Show debug/release mode on startup

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,14 @@ int main(int argc, const char* argv[])
 
 	auto general = &Poco::Logger::get("general");
 	
+#ifdef NDEBUG
+	std::string mode = "Release";
+#else
+	std::string mode = "Debug";
+#endif
+
 	log_information(general, Burst::Settings::Project.nameAndVersionVerbose);
+	log_information(general, "%s mode", mode);
 	log_information(general, "----------------------------------------------");
 	log_information(general, "Github:   https://github.com/Creepsky/creepMiner");
 	log_information(general, "Author:   Creepsky [creepsky@gmail.com]");
