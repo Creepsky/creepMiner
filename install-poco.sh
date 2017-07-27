2d6761ac7a54d65b738970a2b3f524fcc5d47b9e
Creepsky <creepsky@gmail.com>
2017-07-27 09:16:10 +0200

    Removed parallel make

--- install-poco.sh
+++ install-poco.sh
@@ -2,4 +2,4 @@
 set -ex
 wget https://pocoproject.org/releases/poco-1.7.6/poco-1.7.6-all.tar.gz
 tar -xzvf poco-1.7.6-all.tar.gz
-cd poco-1.7.6-all && ./configure --no-tests --no-samples --omit=Data/ODBC,Data/MySQL && make -j $(nproc) && sudo ldconfig && sudo make install
+cd poco-1.7.6-all && ./configure --no-tests --no-samples --omit=Data/ODBC,Data/MySQL && make && sudo ldconfig && sudo make install
