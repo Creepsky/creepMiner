fb52203d2a85abe9b2cb9a0d446c9d2f4669c937
Creepsky <creepsky@gmail.com>
2017-07-29 10:54:15 +0200

    Unlimited buffersize

--- src/plots/PlotReader.cpp
+++ src/plots/PlotReader.cpp
@@ -132,10 +132,15 @@ void Burst::PlotReader::runTask()
 			{
 				//MinerLogger::write("reading plot file " + plotFile.getPath());
 
-				const auto staggerChunkBytes = MinerConfig::getConfig().getMaxBufferSize() * 1024 * 1024 /
-					MinerConfig::getConfig().getBufferChunkCount();
-				const auto staggerChunkRest = plotFile.getStaggerScoopBytes() % staggerChunkBytes;
-				const auto staggerChunks = plotFile.getStaggerScoopBytes() / staggerChunkBytes + (staggerChunkRest > 0 ? 1u : 0u);
+				const auto maxBufferSize = MinerConfig::getConfig().getMaxBufferSize() * 1024 * 1024;
+				auto staggerChunkBytes = maxBufferSize / MinerConfig::getConfig().getBufferChunkCount();
+
+				// unlimited buffer size
+				if (maxBufferSize == 0)
+					staggerChunkBytes = plotFile.getStaggerScoopBytes();
+
+				auto staggerChunkRest = plotFile.getStaggerScoopBytes() % staggerChunkBytes;
+				auto staggerChunks = plotFile.getStaggerScoopBytes() / staggerChunkBytes + (staggerChunkRest > 0 ? 1u : 0u);
 
 				// check, if the incoming plot-read-notification is for the current round
 				currentBlock = plotReadNotification->blockheight == miner_.getBlockheight();
