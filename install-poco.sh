ca22584fa8881445735284149faae5dbf233e3e2
Creepsky <creepsky@gmail.com>
2017-09-04 10:51:08 +0200

    Changed POCO version from 1.7.6 to 1.7.8p3

--- install-poco.sh
+++ install-poco.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://pocoproject.org/releases/poco-1.7.6/poco-1.7.6-all.tar.gz
-tar -xzvf poco-1.7.6-all.tar.gz
-cd poco-1.7.6-all && ./configure --no-tests --no-samples --omit=Data/ODBC,Data/MySQL && make && sudo ldconfig && sudo make install
+wget https://pocoproject.org/releases/poco-1.7.8/poco-1.7.8p3-all.tar.gz
+tar -xzvf poco-1.7.8p3-all.tar.gz
+cd poco-1.7.8p3-all && ./configure --no-tests --no-samples --omit=Data/ODBC,Data/MySQL && make && sudo ldconfig && sudo make install
