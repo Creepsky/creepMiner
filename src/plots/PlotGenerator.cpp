9e6b48018b605b4c2b9eea0d6999ae04c665bd41
Creepsky <creepsky@gmail.com>
2017-09-20 19:06:20 +0200

    Added plot generator

--- src/plots/PlotGenerator.cpp
+++ src/plots/PlotGenerator.cpp
@@ -21,134 +21,60 @@
 
 #include "PlotGenerator.hpp"
 #include "Declarations.hpp"
-#include "MinerShabal.hpp"
-#include "MinerUtil.hpp"
-#include "NonceSubmitter.hpp"
-#include "PlotReader.hpp"
-#include <random>
-#include "MinerLogger.hpp"
-#include <Poco/Net/HTTPClientSession.h>
-#include "Miner.hpp"
+#include "shabal/MinerShabal.hpp"
+#include "mining/Miner.hpp"
 
-Burst::PlotGenerator::PlotGenerator(uint64_t account, uint64_t staggerSize, uint64_t startNonce, uint64_t nonces, void* output)
-	: account_{account}, staggerSize_{staggerSize}, startNonce_{startNonce}, nonces_{nonces}, output_{output}
-{}
-
-void Burst::PlotGenerator::run()
-{
-	for (auto i = 0u; i < nonces_; ++i)
-		generate(account_, staggerSize_, startNonce_, i, output_);
-}
-
-void* Burst::PlotGenerator::getOutput() const
-{
-	return output_;
-}
-
-void* Burst::PlotGenerator::generate(uint64_t account, uint64_t staggerSize, uint64_t startNonce, uint64_t cachePos, void* output)
+Poco::UInt64 Burst::PlotGenerator::generateAndCheck(Poco::UInt64 account, Poco::UInt64 nonce, const Miner& miner)
 {
 	char final[32];
 	char gendata[16 + Settings::PlotSize];
-	Shabal256 shabal;
-	//auto mem = calloc(Settings::PlotSize, staggerSize);
-	auto cache = static_cast<char*>(output);
 
-	auto *xv = reinterpret_cast<char*>(&account);
+	auto xv = reinterpret_cast<char*>(&account);
 
-	gendata[Settings::PlotSize] = xv[7]; gendata[Settings::PlotSize + 1] = xv[6]; gendata[Settings::PlotSize + 2] = xv[5]; gendata[Settings::PlotSize + 3] = xv[4];
-	gendata[Settings::PlotSize + 4] = xv[3]; gendata[Settings::PlotSize + 5] = xv[2]; gendata[Settings::PlotSize + 6] = xv[1]; gendata[Settings::PlotSize + 7] = xv[0];
+	for (auto j = 0u; j <= 7; ++j)
+		gendata[Settings::PlotSize + j] = xv[7 - j];
 
-	xv = reinterpret_cast<char*>(&startNonce);
+	xv = reinterpret_cast<char*>(&nonce);
 
-	gendata[Settings::PlotSize + 8] = xv[7]; gendata[Settings::PlotSize + 9] = xv[6]; gendata[Settings::PlotSize + 10] = xv[5]; gendata[Settings::PlotSize + 11] = xv[4];
-	gendata[Settings::PlotSize + 12] = xv[3]; gendata[Settings::PlotSize + 13] = xv[2]; gendata[Settings::PlotSize + 14] = xv[1]; gendata[Settings::PlotSize + 15] = xv[0];
-	
+	for (auto j = 0u; j <= 7; ++j)
+		gendata[Settings::PlotSize + 8 + j] = xv[7 - j];
+
+	sph_shabal256_context x;
+
 	for (auto i = Settings::PlotSize; i > 0; i -= Settings::HashSize)
 	{
-		shabal = {};
+		sph_shabal256_init(&x);
 
 		auto len = Settings::PlotSize + 16 - i;
 
 		if (len > Settings::ScoopPerPlot)
 			len = Settings::ScoopPerPlot;
 
-		shabal.update(&gendata[i], len);
-		shabal.close(&gendata[i - Settings::HashSize]);
+		sph_shabal256(&x, &gendata[i], len);
+		sph_shabal256_close(&x, &gendata[i - Settings::HashSize]);
 	}
 
-	shabal = {};
-	shabal.update(gendata, 16 + Settings::PlotSize);
-	shabal.close(&final);
+	sph_shabal256_init(&x);
+	sph_shabal256(&x, gendata, 16 + Settings::PlotSize);
+	sph_shabal256_close(&x, final);
 
 	// XOR with final
-	for (auto i = 0u; i < Settings::PlotSize; i++)
-		gendata[i] ^= (final[i % 32]);
+	for (auto i = 0; i < Settings::PlotSize; i++)
+		gendata[i] ^= final[i % 32];
 
-	// Sort them:
-	for (auto i = 0u; i < Settings::PlotSize; i += 64)
-		memmove(&cache[cachePos * 64 + static_cast<unsigned long long>(i) * staggerSize], &gendata[i], 64);
-	
-	return cache;
-}
+	std::array<uint8_t, 32> target;
+	Poco::UInt64 result;
 
-Burst::RandomNonceGenerator::RandomNonceGenerator(Miner& miner, uint64_t account, uint64_t staggerSize, uint64_t randomNonces)
-	: Task("RandomNonceGenerator"), miner_{&miner}, account_{account}, staggerSize_{staggerSize}, randomNonces_{randomNonces}
-{}
+	const auto generationSignature = miner.getGensig();
+	const auto scoop = miner.getScoopNum();
+	const auto basetarget = miner.getBaseTarget();
 
-struct membuf : std::streambuf
-{
-	membuf(char* begin, char* end) {
-		this->setg(begin, begin, end);
-	}
-};
+	sph_shabal256_init(&x);
+	sph_shabal256(&x, generationSignature.data(), Settings::HashSize);
+	sph_shabal256(&x, &gendata[scoop * Settings::ScoopSize], Settings::ScoopSize);
+	sph_shabal256_close(&x, target.data());
 
-void Burst::RandomNonceGenerator::runTask()
-{
-	std::default_random_engine rd;
-	std::mt19937 mt(rd());
-	std::uniform_int_distribution<uint64_t> dist;
-	
-	auto mem = calloc(Settings::PlotSize, static_cast<size_t>(staggerSize_));
-	auto gensig = miner_->getGensig();
+	memcpy(&result, target.data(), sizeof(Poco::UInt64));
 
-	std::vector<ScoopData> buffer;
-	buffer.resize(1);
-
-	auto scoopData = reinterpret_cast<char*>(&buffer[0]);
-
-	for (auto i = 0u; (i < randomNonces_ || randomNonces_ == 0) && !isCancelled(); ++i)
-	{
-		//auto startNonce = dist(mt);
-
-		//PlotGenerator plotGenerator{account_, staggerSize_, startNonce, 1, mem};
-		//plotGenerator.run();
-
-		////PlotGenerator::generate(account_, staggerSize_, startNonce, mem);
-
-		//auto begin = reinterpret_cast<char*>(mem);
-		////auto end = begin + Settings::PlotSize * staggerSize_;
-		//
-		//Shabal256 hash;
-		//std::istringstream{begin}.read(scoopData, staggerSize_);
-
-		//HashData target;
-		//auto test = buffer.data();
-		//hash.update(gensig.data(), Settings::HashSize);
-		//hash.update(test, Settings::ScoopSize);
-		//hash.close(&target[0]);
-
-		//uint64_t targetResult = 0;
-		//memcpy(&targetResult, &target[0], sizeof(decltype(targetResult)));
-		//auto deadline = targetResult / miner_->getBaseTarget();
-
-		//auto nonceNum = startNonce;
-		//miner_->submitNonce(nonceNum, account_, deadline, "");
-
-		//PlotReader plotReader{ *miner_, std::make_unique<std::istringstream>(begin), account_, startNonce,
-			//1, staggerSize_ };
-		//plotReader.runTask();
-	}
-
-	log_debug(MinerLogger::general, "Random nonce generator finished...");
-	free(mem);
+	return result / basetarget;
 }
--- src/plots/PlotGenerator.hpp
+++ src/plots/PlotGenerator.hpp
@@ -21,44 +21,15 @@
 
 #pragma once
 
-#include <cstdint>
-#include <Poco/Task.h>
+#include <Poco/Types.h>
 
 namespace Burst
 {
 	class Miner;
 
-	class PlotGenerator : public Poco::Runnable
+	class PlotGenerator
 	{
 	public:
-		PlotGenerator(Poco::UInt64 account, Poco::UInt64 staggerSize, Poco::UInt64 startNonce, Poco::UInt64 nonces, void* output = nullptr);
-		~PlotGenerator() override = default;
-
-		void run() override;
-		void* getOutput() const;
-
-		static void* generate(Poco::UInt64 account, Poco::UInt64 staggerSize, Poco::UInt64 startNonce, Poco::UInt64 cachePos, void* output);
-
-	private:
-		Poco::UInt64 account_;
-		Poco::UInt64 staggerSize_;
-		Poco::UInt64 startNonce_;
-		Poco::UInt64 nonces_;
-		void* output_;
-	};
-
-	class RandomNonceGenerator : public Poco::Task
-	{
-	public:
-		RandomNonceGenerator(Miner& miner, Poco::UInt64 account, Poco::UInt64 staggerSize, Poco::UInt64 randomNonces);
-		~RandomNonceGenerator() override = default;
-
-		void runTask() override;
-
-	private:
-		Miner* miner_;
-		Poco::UInt64 account_;
-		Poco::UInt64 staggerSize_;
-		Poco::UInt64 randomNonces_;
+		static Poco::UInt64 generateAndCheck(Poco::UInt64 account, Poco::UInt64 nonce, const Miner& miner);
 	};
 }
