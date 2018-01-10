560dc9d63147ba62bd77eae82c60d52aa108e159
Unknown <creepsky@gmail.com>
2018-01-10 16:54:23 +0100

    Adapt to new conan version

--- install.sh
+++ install.sh
@@ -88,7 +88,7 @@ echo $use_avx2
 echo $use_opencl
 echo $use_cuda
 
-conan install --build=missing -s compiler.libcxx=libstdc++11
+conan install . --build=missing -s compiler.libcxx=libstdc++11
 rm CMakeCache.txt -f
 cmake . -DCMAKE_BUILD_TYPE=RELEASE $use_sse4 $use_avx $use_avx2 $use_opencl $use_cuda
 make -j$(nproc)
