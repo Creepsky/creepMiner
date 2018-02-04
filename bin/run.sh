fbf7de2a34f9f8a7dd67e73799bbfcd6891abda0
Unknown <creepsky@gmail.com>
2018-02-04 13:33:16 +0100

    Moved, changed.. whatever

--- bin/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-export LD_LIBRARY_PATH
-export LC_ALL="en_US.UTF-8"
-cd "$(dirname "$0")"
-./creepMiner
--- bin/screen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh 
-cd "$(dirname "$0")" 
-screen -dmS creep ./run.sh
