f7a95d4f327048692522478fea85f77160b1357a
Herscht <Herscht@gmx.de>
2018-03-10 01:42:50 +0100

    BugFix
    - replaced blockTargetDeadline with getTargetDeadline() in startNewBlock().

--- src/mining/Miner.cpp
+++ src/mining/Miner.cpp
@@ -303,7 +303,7 @@ void Burst::Miner::updateGensig(const std::string& gensigStr, Poco::UInt64 block
 	}
 
 	// setup new block-data
-	auto block = data_.startNewBlock(blockHeight, baseTarget, gensigStr, blockTargetDeadline);
+	auto block = data_.startNewBlock(blockHeight, baseTarget, gensigStr, MinerConfig::getConfig().getTargetDeadline(TargetDeadlineType::Local));
 
 	// printing block info and transfer it to local server
 	{
