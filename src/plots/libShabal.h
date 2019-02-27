a8a882cd8f3e3384701ee229c1b5352f0416ea8d
Harry Phillips <harryjamesphillips@gmail.com>
2019-02-27 21:05:01 +0000

    Remove strangely exported function

--- src/plots/libShabal.h
+++ src/plots/libShabal.h
@@ -4,12 +4,6 @@
 
 extern "C" {
 
-extern void find_best_deadline_sph(const char *scoops,
-                                   uint64_t nonce_count,
-                                   const char *gensig,
-                                   uint64_t *best_deadline,
-                                   uint64_t *best_offset);
-
 uint64_t shabal_findBestDeadline(const char *scoops, uint64_t nonce_count, const char *gensig);
 
 void shabal_findBestDeadlineDirect(const char *scoops,
